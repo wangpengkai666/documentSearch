14b579523ee155ab08aedd3e74259a4cbea75ca0
wangpengkai <15659265808@163.com>
2022-06-29 17:44:07 +0800

    业务增加: (1)完善模板并发预创建 (2)丰富了模板dao类的方法

--- src/main/java/com/example/documentseach/persistent/dao/es/IndexESDAO.java
+++ src/main/java/com/example/documentseach/persistent/dao/es/IndexESDAO.java
@@ -1,10 +1,46 @@
 package com.example.documentseach.persistent.dao.es;
 
+import com.alibaba.fastjson2.JSONObject;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author wangpengkai
  */
 @Repository
-public class IndexESDAO extends BaseESDAO{
+public class IndexESDAO extends BaseESDAO {
+    /**
+     * @return
+     */
+    public boolean createIndex() {
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    public boolean deleteIndex() {
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    public boolean updateIndexSettings() {
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    public boolean updateIndexMappings() {
+        return false;
+    }
+
+    /**
+     * @param indexName
+     * @return
+     */
+    public JSONObject getIndexStructure(String indexName) {
+        return null;
+    }
 }
--- src/main/java/com/example/documentseach/task/TemplateCreateTask.java
+++ src/main/java/com/example/documentseach/task/TemplateCreateTask.java
@@ -2,6 +2,7 @@ package com.example.documentseach.task;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.example.documentseach.common.util.GetJSONFromTxt;
+import com.example.documentseach.common.util.RetryTimesOp;
 import com.example.documentseach.common.util.log.KLog;
 import com.example.documentseach.common.util.log.LoggerFactory;
 import com.example.documentseach.persistent.dao.es.TemplateESDAO;
@@ -17,36 +18,61 @@ import java.util.stream.Collectors;
  * @author wangpengkai
  */
 @Configuration
-public class TemplateCreateTask {
+public class TemplateCreateTask extends ConcurrentBaseTask<String> {
     private static final KLog LOGGER = LoggerFactory.getLog(TemplateCreateTask.class);
 
     @Autowired
     private TemplateESDAO templateESDAO;
 
-    private Set<String> preCreateTemplateName;
+    private List<String> preCreateTemplateName;
+
+    private final Integer current = 5;
+
+    private final String taskName = "templateCreateTask";
 
     @PostConstruct
-    public void init() {
-        try {
-            // 1.获取预创建模板目录下的所有name
-            File file = new File("src/main/java/com/example/documentseach/default/template");
-            preCreateTemplateName = Arrays.stream(file.listFiles()).map(File::getName).collect(Collectors.toSet());
+    public void init0() {
+        this.execute();
+    }
 
-            // 2.开始创建所有的初始化预创建模板
-            List<String> failCreateTemplateNames = new LinkedList<>();
-            for (String templateName : preCreateTemplateName) {
+    @Override
+    protected List<String> getAllItems() {
+        File file = new File("src/main/java/com/example/documentseach/default/template");
+        preCreateTemplateName = Arrays.stream(file.listFiles()).map(File::getName).collect(Collectors.toList());
+        return preCreateTemplateName;
+    }
+
+    @Override
+    protected boolean executeByBatch(TaskBatch<String> templateNames) throws Exception {
+        List<String> failCreateTemplateNames = new LinkedList<>();
+        for (String templateName : templateNames.getItems()) {
+            // 创建对应模板并且进行重试操作
+            RetryTimesOp.esRetryExecute("createTemplate", 3, () -> {
                 JSONObject templateStructure = GetJSONFromTxt.readDslFileInJarFile(templateName);
-                boolean template = templateESDAO.createTemplate(templateName,
+                return templateESDAO.createTemplate(templateName,
                         templateStructure.getJSONArray("index_patterns").toList(String.class),
                         templateStructure.getJSONObject("mappings"),
                         templateStructure.getJSONObject("settings")
                 );
-
-                if (!template) failCreateTemplateNames.add(templateName);
-            }
-            LOGGER.warn("class=TemplateCreateTask||method=init||the fail add templates is {}", failCreateTemplateNames);
-        } catch (Exception e) {
-            LOGGER.error("class=TemplateCreateTask||method=init||errMsg={}", e.getMessage());
+            });
         }
+
+        LOGGER.warn("class=TemplateCreateTask||method=executeByBatch||the fail add templates is {}", failCreateTemplateNames);
+        return true;
+    }
+
+    @Override
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    @Override
+    public int poolSize() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    @Override
+    public int current() {
+        return this.current;
     }
 }
