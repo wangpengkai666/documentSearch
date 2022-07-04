34956af4227d1ea43ba487fb5f1d999103ac3c16
wangpengkai <15659265808@163.com>
2022-07-05 01:12:15 +0800

    框架增加: (1)整合了swagger接口文档说明框架

---
+++ src/main/java/com/example/documentseach/rest/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.example.documentseach.rest.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author wangpengkai
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.documentseach.rest.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该Api的基本信息（这些基本信息会展现在文档页面中）
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("DocumentSearch接口文档")
+                .description("test")
+                .version("1.0_")
+                .title("接口文档")
+                .license("Apache2.0")
+                .licenseUrl("http://www.apche.org/licenses/LICENSE-2.0")
+                .build();
+    }
+}
